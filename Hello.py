59463e79c4f76bed0deee238baf88d3469490b39
Pimchanok Sonta <165460230+nnut21@users.noreply.github.com>
2025-01-06 18:33:01 +0700

    Main

---
+++ Hello.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Accident Tracking Dashboard! 👋")
+
+st.sidebar.success("Select a pages above.")
+
+st.markdown(
+    """
+    Traditional accident reporting methods, like documents, meetings, and websites, face **time constraints** due to lengthy data collection and analysis, delaying decisions. 
+
+    This project uses **AI** and **dashboards** to accelerate analysis, enabling **faster decisions** and providing **flexible, user-friendly visualizations**.
+
+    ### Who is a member?
+    - **6610422034** Kittipong Morsuya 👨🏻‍💻
+    - **6620422010** Natakamolwan Daengpairoj 👩🏻‍💻
+    - **6620422016** Teerata Srikhiewphong 👩🏻‍💻
+    - **6620422018** Pimchanok Sonta 👨🏻‍💻
+    """
+)
+
+# 6610422034 Kittipong Morsuya
+# 6620422010 Natakamolwan Daengpairoj
+# 6620422016 Teerata Srikhiewphong
+# 6620422018 Pimchanok Sonta
+
+# Who is a member?
+#     - Ask a question in our [community
+#         forums](https://discuss.streamlit.io)
+#     ### See more complex demos
+#     - Use a neural net to [analyze the Udacity Self-driving Car Image
+#         Dataset](https://github.com/streamlit/demo-self-driving)
+#     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
